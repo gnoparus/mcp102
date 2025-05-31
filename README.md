75cd6bc67c24a315e462831cb1eaa3711f5a7e77
Surapong Kanoktipsatharporn <surapong@gmail.com>
2025-05-31 13:14:14 +0700

    init import

---
+++ README.md
@@ -0,0 +1,2 @@
+
+1. start with chatbot_tools_calling.ipynb for standard tools calling before refactor to mcp
---
+++ main.py
@@ -0,0 +1,7 @@
+
+def main():
+    print("Hello, World!")
+    # Add your main logic here
+
+if __name__ == "__main__":
+    main()
